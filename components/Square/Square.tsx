dda24e5c788ad0fea3483c6aa5f706e94651ee90
Michael Van Delft <michael@hybr.id.au>
2023-01-24 19:37:52 +0800

    Attach a function to the squares

--- components/Square/Square.tsx
+++ components/Square/Square.tsx
@@ -1,22 +1,35 @@
+import { MouseEventHandler } from "react";
 import styles from "./Square.module.css";
 
 export enum Color {
   red = "#FF0000",
   green = "#00FF00",
   blue = "#0000FF",
-  white = "#FFFFFF", // None
+  white = "#FFFFFF",
+  none = "#00000000", // rgba transparent
+}
+
+export enum Modifier {
+  none = "none",
+  left = "left",
+  up = "up",
+  right = "right",
+  down = "down",
 }
 
 type SquareProps = {
   color: Color;
   targetColor: Color;
+  modifier: Modifier;
+  onClick: MouseEventHandler;
 };
 
 export function Square(props: SquareProps) {
   return (
-    <td
+    <button
       className={styles.square}
       style={{ backgroundColor: props.color, borderColor: props.targetColor }}
-    ></td>
+      onClick={props.onClick}
+    />
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,62 @@
 import styles from "./index.module.css";
-import { Square, Color } from "@/components/Square/Square";
+import { Square, Color, Modifier } from "@/components/Square/Square";
 import Head from "./head";
 import { useState } from "react";
 
 export default function Home() {
   const [game, setGame] = useState([
     [
-      <Square color={Color.red} targetColor={Color.red} />,
-      <Square color={Color.white} targetColor={Color.red} />,
-      <Square color={Color.white} targetColor={Color.red} />,
+      <Square
+        color={Color.red}
+        targetColor={Color.red}
+        modifier={Modifier.right}
+        onClick={() => updateGame(0, 0)}
+      />,
+      <Square
+        color={Color.white}
+        targetColor={Color.red}
+        modifier={Modifier.none}
+        onClick={() => updateGame(0, 1)}
+      />,
+      <Square
+        color={Color.none}
+        targetColor={Color.none}
+        modifier={Modifier.none}
+        onClick={() => updateGame(0, 2)}
+      />,
+    ],
+    [
+      <Square
+        color={Color.red}
+        targetColor={Color.red}
+        modifier={Modifier.right}
+        onClick={() => updateGame(1, 0)}
+      />,
+      <Square
+        color={Color.white}
+        targetColor={Color.red}
+        modifier={Modifier.none}
+        onClick={() => updateGame(1, 1)}
+      />,
+      <Square
+        color={Color.none}
+        targetColor={Color.none}
+        modifier={Modifier.none}
+        onClick={() => updateGame(1, 2)}
+      />,
     ],
   ]);
+
+  function updateGame(x: number, y: number) {
+    if ( game[x][y].props.modifier === Modifier.right ) {
+      alert("Right")
+    }
+  }
   return (
     <>
       <Head />
       <main className={styles.main}>
-        <table>
-          <tbody>
-            <tr>{game}</tr>
-          </tbody>
-        </table>
+        <div>{game}</div>
       </main>
     </>
   );
