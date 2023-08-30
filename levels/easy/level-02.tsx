bedd8c6c35fe10fd9858fdd307a4cd026b616b7b
Michael Van Delft <michael@hybr.id.au>
2023-08-30 19:21:31 +0800

    Add level 2 in preparation for being able to change levels

---
+++ levels/easy/level-02.tsx
@@ -0,0 +1,141 @@
+// This is just a place holder to test with,
+// later we will load all the levels by parsing the xml.
+import { Color, Modifier, SquareProps } from "@/components/Square/Square";
+
+export const level: Array<Array<SquareProps>> = [
+  [
+    {
+      color: Color.none,
+      targetColor: Color.blue,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+  ],
+  [
+    {
+      color: Color.none,
+      targetColor: Color.blue,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+  ],
+  [
+    {
+      color: Color.none,
+      targetColor: Color.blue,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.red,
+      targetColor: Color.red,
+      modifier: Modifier.circle,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+  ],
+  [
+    {
+      color: Color.none,
+      targetColor: Color.blue,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+  ],
+  [
+    {
+      color: Color.blue,
+      targetColor: Color.blue,
+      modifier: Modifier.up,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+    {
+      color: Color.none,
+      targetColor: Color.red,
+      modifier: Modifier.none,
+    },
+  ],
+];
