30fab55e7fef5061d3fddc3cf573be61caa3205a
Michael Van Delft <michael@hybr.id.au>
2023-09-23 23:11:02 +0800

    Add an intentionally broken test to make sure Actions are working correctly.

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -10,4 +10,12 @@ describe("Home Screen", () => {
     // This is a bit of a silly test, but it's just a proof of concept to make sure that jest works.
     expect(screen.getByTestId("Easy-pack-button")).toBeInTheDocument();
   });
+  it("Broken test", () => {
+    render(<Home />);
+    // Intentionally broken test. To make sure that failed tests actually fail the PR.
+    // It wouldn't be the first time I've seen pipelines that "ran successfully"
+    // only to find in the output, everything failed. But for various reasons the
+    // final exit code is 0, so it shows success.
+    expect(screen.getByTestId("Intentionally broken test")).toBeInTheDocument();
+  });
 });
