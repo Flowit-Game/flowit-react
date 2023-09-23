55050570928020e082fbb5adcb345e5c3a222b8e
Michael Van Delft <michael@hybr.id.au>
2023-09-23 22:58:34 +0800

    Run jest tests on PRs

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,17 @@
-// Copy/pasted from https://nextjs.org/docs/pages/building-your-application/optimizing/testing#setting-up-jest-with-the-rust-compiler
 import nextJest from 'next/jest'
+import {Config} from 'jest'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
 
-// Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
-const config = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
+const config: Config = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': ['<rootDir>/$1'],
   },
 }
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+// createJestConfig is exported this way to ensure that
+// next/jest can load the Next.js config which is async
 export default createJestConfig(config)
